cf7e2d8684599ed56f74a0b26a89e388970e1a47
Jan Brohl <janbrohl@t-online.de>
2015-11-01 16:50:51 +0100

    moved code to functions, use regex, move error-message to error.html, use html5

--- application.py
+++ application.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
 from urlparse import parse_qsl
 from xml.sax.saxutils import escape, quoteattr
 from wsgiref.util import FileWrapper
+import re
+
+__version__ = "0.2"
+
+url_re = re.compile("url(?::(.+))?")
+metaurl_re = re.compile("metaurl:(.+)")
+hash_re = re.compile("hash:(.+)")
 
 
 def _first_qst(qst, key):
@@ -11,6 +20,10 @@ def _first_qst(qst, key):
     return None
 
 
+def _getname(qst):
+    return quoteattr(_first_qst(qst, "name"))
+
+
 def _getsize(qst):
     size = _first_qst(qst, "size")
     if size:
@@ -18,38 +31,69 @@ def _getsize(qst):
     return ""
 
 
+def _geturls(qst):
+    outl = []
+    for k, s in qst:
+        m = url_re.match(k)
+        if m is not None:
+            if m.group(1):
+                outl.append("<url location=%s>%s</url>" %
+                            (quoteattr(m.group(1)), escape(s)))
+            else:
+                outl.append("<url>%s</url>" % escape(s))
+    return "\n".join(outl)
+
+
+def _getmetaurls(qst):
+    outl = []
+    for k, s in qst:
+        m = metaurl_re.match(k)
+        if m is not None:
+            outl.append('<metaurl mediatype=%s>%s</metaurl>' %
+                        (quoteattr(m.group(1)), escape(s)))
+    return "\n".join(outl)
+
+
+def _gethashes(qst):
+    outl = []
+    for k, s in qst:
+        m = hash_re.match(k)
+        if m is not None:
+            outl.append('<hash type=%s>%s</hash>' %
+                        (quoteattr(m.group(1)), escape(s)))
+    return "\n".join(outl)
+
+
 def makelink(qst):
     return """<?xml version="1.0" encoding="UTF-8"?>
 <metalink xmlns="urn:ietf:params:xml:ns:metalink">
-<generator>ToMeta/0.1</generator>
-<file name=%s>%s%s%s%s
+<generator>ToMeta/{version}</generator>
+<file name={name}>
+{size}
+{urls}
+{metaurls}
+{hashes}
 </file>
-</metalink>""" % (quoteattr(_first_qst(qst, "name")),
-                  _getsize(qst),
-                  "".join((("<url>%s</url>" % escape(s)) if ":"not in k else ("<url location=%s>%s</url>" %
-                                                                              (quoteattr(k.split(":")[1]), escape(s)))) for k, s in qst if k.startswith("url")),
-                  "".join(('<metaurl mediatype=%s>%s</metaurl>' % (quoteattr(k.split(":")
-                                                                             [1]), escape(s))) for k, s in qst if k.startswith("metaurl:")),
-                  "".join(('<hash type=%s>%s</hash>' % (quoteattr(k.split(":")
-                                                                  [1]), escape(s))) for k, s in qst if k.startswith("hash:"))
-                  )
+</metalink>""".format(version=__version__, name=_getname(qst),
+                      size=_getsize(qst), urls=_geturls(qst),
+                      metaurls=_getmetaurls(qst), hashes=_gethashes(qst))
 
 
 def application(environ, start_response):
     qst = tuple(parse_qsl(environ["QUERY_STRING"]))
     if not qst:
         status = '200 OK'  # HTTP Status
-        headers = [('Content-type', 'text/html')]
+        headers = [('Content-type', 'text/html; charset=utf-8')]
         out = FileWrapper(open("form.html"))
     else:
         try:
-            out = makelink(qst)
+            out = [makelink(qst)]
             status = '200 OK'  # HTTP Status
             # HTTP Headers
             headers = [('Content-type', 'application/metalink4+xml')]
         except Exception, e:
-            out = "Bad query string!\nRequired: name\nOptional: size\nAny count: url, url:[location], hash:[type], metaurl:[mediatype]"
+            out = FileWrapper(open("form.html"))
             status = "400 Bad Request"
-            headers = [('Content-type', 'text/plain')]
+            headers = [('Content-type', 'text/html; charset=utf-8')]
     start_response(status, headers)
-    return [out]
+    return out
