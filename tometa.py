df43a5f5e1920deb19ba63c00c775e29feb9fe1c
Jan Brohl <janbrohl@t-online.de>
2015-11-06 15:01:15 +0100

    move functions into class

--- tometa.py
+++ tometa.py
@@ -6,80 +6,13 @@ from xml.sax.saxutils import escape, quoteattr
 from wsgiref.util import FileWrapper
 import re
 
-__version__ = "0.2"
-
-url_re = re.compile("url(?::(.+))?")
-metaurl_re = re.compile("metaurl:(.+)")
-hash_re = re.compile("hash:(.+)")
-
-
-def _first_qst(qst, key):
-    for k, v in qst:
-        if k == key:
-            return v
-    return None
-
-
-def _getname(qst):
-    return quoteattr(_first_qst(qst, "name"))
-
-
-def _getsize(qst):
-    size = _first_qst(qst, "size")
-    if size:
-        return "<size>%i</size>" % int(size)
-    return ""
-
-
-def _geturls(qst):
-    outl = []
-    for k, s in qst:
-        m = url_re.match(k)
-        if m is not None:
-            if m.group(1):
-                outl.append("<url location=%s>%s</url>" %
-                            (quoteattr(m.group(1)), escape(s)))
-            else:
-                outl.append("<url>%s</url>" % escape(s))
-    return "\n".join(outl)
-
-
-def _getmetaurls(qst):
-    outl = []
-    for k, s in qst:
-        m = metaurl_re.match(k)
-        if m is not None:
-            outl.append('<metaurl mediatype=%s>%s</metaurl>' %
-                        (quoteattr(m.group(1)), escape(s)))
-    return "\n".join(outl)
-
-
-def _gethashes(qst):
-    outl = []
-    for k, s in qst:
-        m = hash_re.match(k)
-        if m is not None:
-            outl.append('<hash type=%s>%s</hash>' %
-                        (quoteattr(m.group(1)), escape(s)))
-    return "\n".join(outl)
-
-
-def makelink(qst):
-    return """<?xml version="1.0" encoding="UTF-8"?>
-<metalink xmlns="urn:ietf:params:xml:ns:metalink">
-<generator>ToMeta/{version}</generator>
-<file name={name}>
-{size}
-{urls}
-{metaurls}
-{hashes}
-</file>
-</metalink>""".format(version=__version__, name=_getname(qst),
-                      size=_getsize(qst), urls=_geturls(qst),
-                      metaurls=_getmetaurls(qst), hashes=_gethashes(qst))
+__version__ = "0.3"
 
 
 class App(object):
+    url_re = re.compile("url(?::(.+))?")
+    metaurl_re = re.compile("metaurl:(.+)")
+    hash_re = re.compile("hash:(.+)")
 
     def __init__(self, form_path="form.html", error_path="error.html"):
         self.form_path = form_path
@@ -104,5 +37,68 @@ class App(object):
         start_response(status, headers)
         return out
 
+    @staticmethod
+    def first_qst(qst, key):
+        for k, v in qst:
+            if k == key:
+                return v
+        return None
 
-application = App()
+    @classmethod
+    def getname(cls, qst):
+        return quoteattr(qls.first_qst(qst, "name"))
+
+    @classmethod
+    def getsize(cls, qst):
+        size = cls.first_qst(qst, "size")
+        if size:
+            return "<size>%i</size>" % int(size)
+        return ""
+
+    @classmethod
+    def geturls(cls, qst):
+        outl = []
+        for k, s in qst:
+            m = cls.url_re.match(k)
+            if m is not None:
+                if m.group(1):
+                    outl.append("<url location=%s>%s</url>" %
+                                (quoteattr(m.group(1)), escape(s)))
+                else:
+                    outl.append("<url>%s</url>" % escape(s))
+        return "\n".join(outl)
+
+    @classmethod
+    def getmetaurls(cls, qst):
+        outl = []
+        for k, s in qst:
+            m = cls.metaurl_re.match(k)
+            if m is not None:
+                outl.append('<metaurl mediatype=%s>%s</metaurl>' %
+                            (quoteattr(m.group(1)), escape(s)))
+        return "\n".join(outl)
+
+    @classmethod
+    def gethashes(cls, qst):
+        outl = []
+        for k, s in qst:
+            m = cls.hash_re.match(k)
+            if m is not None:
+                outl.append('<hash type=%s>%s</hash>' %
+                            (quoteattr(m.group(1)), escape(s)))
+    return "\n".join(outl)
+
+    @classmethod
+    def makelink(cls, qst):
+        return """<?xml version="1.0" encoding="UTF-8"?>
+<metalink xmlns="urn:ietf:params:xml:ns:metalink">
+<generator>ToMeta/{version}</generator>
+<file name={name}>
+{size}
+{urls}
+{metaurls}
+{hashes}
+</file>
+</metalink>""".format(version=__version__, name=cls.getname(qst),
+                      size=cls.getsize(qst), urls=cls.geturls(qst),
+                      metaurls=cls.getmetaurls(qst), hashes=cls.gethashes(qst))
