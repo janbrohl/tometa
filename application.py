b06cdb3c34b9a10bd7d209ee44f91c724f5feb2d
Jan Brohl <janbrohl@t-online.de>
2016-07-19 17:40:01 +0200

    add demo server

--- application.py
+++ application.py
@@ -1,3 +1,8 @@
 from tometa import App
 
 application = App()
+
+if __name__ == "__main__":
+    import wsgiref.simple_server
+    serv = wsgiref.simple_server.make_server("localhost", 8080, application)
+    serv.serve_forever()
